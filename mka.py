8b92fcf0efdf5ff6ed2ea85a0089693f9a7d756c
vkondula <vkondula@redhat.com>
2016-04-05 13:15:20 +0200

    Version 0.2.1

--- mka.py
+++ mka.py
@@ -4,9 +4,13 @@
 import os
 import sys
 import argparse
-import stateMachine
+import stateMachine as SM
 from configparser import ConfigParser
-from stateMachine import FiniteStateMachine as FST
+from stateMachine import FiniteStateMachine as FSM
+
+CLASSIC_FSM = "classic.conf"
+RULES_ONLY_FSM = "rules_only.conf"
+WHITE_CHAR_FSM = "white_char.conf"
 
 
 def parse_args(parser):
@@ -26,8 +30,8 @@ def parse_args(parser):
         help="Prints out all non finishing states, '0' if no state is found",
         action="store_true")
     group.add_argument(
-        "-m", "--minimaze",
-        help="Minimaze finite state machine",
+        "-m", "--minimize",
+        help="Minimize finite state machine",
         action="store_true")
     parser.add_argument(
         "-i", "--case-insensitive",
@@ -44,7 +48,8 @@ def parse_args(parser):
     group.add_argument(
         "--analyze-string",
         help="Check if given string can be read by state machine",
-        type=str)
+        type=str,
+        default=None)
     parser.add_argument(
         "--wsfa",
         help="Simple deterministic finite state machine could be given",
@@ -75,62 +80,105 @@ def parse_args(parser):
     return args
 
 
-def input_parsing_fst(conf_path, starting_state, fd, until_eof):
-    state_fst = FST(rules_only=True)
+def input_parsing_fsm(conf_path, starting_state, fd):
+    state_fsm = FSM(rules_only=True)
     conf = ConfigParser()
     conf.read(conf_path)
-    state_fst.build_from_config(conf)
-    state_fst.set_starting(starting_state)
-    while not state_fst.is_finishing() or until_eof:
+    state_fsm.build_from_config(conf)
+    state_fsm.set_starting(starting_state)
+    while not state_fsm.is_finishing():
         char = fd.read(1)
         if char == "":
             break
-        state_fst.step(char)
-    if not state_fst.is_finishing():
-        raise stateMachine.NotFinishing()
-    return state_fst.get_output()
+        state_fsm.step(char)
+    if not state_fsm.is_finishing():
+        raise SM.NotFinishing("Lexial or syntax error", "\n")
+    return state_fsm.get_output()
+
+def createFSM(configuration, case_insensitive = False, rules_only = False):
+    list_of_states = configuration.get("state", [])
+    list_of_symbols = configuration.get("symbol", [])
+    list_of_rules = zip(
+        configuration.get("current", []),
+        configuration.get("target", []),
+        configuration.get("input", [])
+    )
+    list_of_finishing = configuration.get("finish", [])
+    start = configuration.get("start").pop()
+    print(list_of_states)
+    print(list_of_symbols)
+    print("start: %s" % start)
+    print(list_of_finishing)
+    fsm = FSM(rules_only = rules_only)
+    # add all states
+    for item in list_of_states:
+        if case_insensitive:
+            item = item.lower()
+        fsm.add_state(item)
+    # add all symbols
+    if not list_of_symbols:
+        raise SM.Invalid("Set of symbols is empty", "\n")
+    for item in list_of_symbols:
+        if case_insensitive:
+            item = item.lower()
+        fsm.add_symbol(item)
+    # add rules
+    for item in list_of_rules:
+        if case_insensitive:
+            fsm.add_rule(item[0].lower(), item[1].lower(), item[2].lower())
+        else:
+            fsm.add_rule(item[0], item[1], item[2])
+    # set starting
+    fsm.set_starting(start)
+    # set finishing
+    for item in list_of_finishing:
+        if case_insensitive:
+            item = item.lower()
+        fsm.set_finishing(item)
+    return fsm
 
 
 def main(args):
-    if not args.rules_only:
-        states = input_parsing_fst(
-            "state.conf", "start", args.input, False)
-        list_of_states = states.get("1", [])
+    if args.rules_only:
+        fsm_conf = RULES_ONLY_FSM
+    if args.white_char:
+        fsm_conf = WHITE_CHAR_FSM
+    else:
+        fsm_conf = CLASSIC_FSM
+    # Prepare input readinf FSM
+    try:
+        config = input_parsing_fsm(fsm_conf, "start_1", args.input)
+    except (SM.NotFinishing, SM.MissingRule) as e:
+        sys.stderr.write(" ".join(e.args))
+        sys.exit(60)
 
-        alphabet = input_parsing_fst(
-            "alphabet.conf", "start", args.input, False)
-        list_of_symbols = alphabet.get("2", [])
+    # Fill FSM with states, symbols and rules
+    try:
+        fsm = createFSM(config, args.case_insensitive, args.rules_only)
+    except (SM.Invalid) as e:
+        sys.stderr.write(" ".join(e.args))
+        sys.exit(61)
 
-        rules = input_parsing_fst("rules.conf", "start", args.input, False)
-        list_of_rules = zip(
-            rules.get("state", []),
-            rules.get("target", []),
-            rules.get("symbol", [])
-        )
+    if args.wsfa:
+        # TODO
+        pass
 
-        finishing = input_parsing_fst("finishing.conf", "start", args.input, True)
-        list_of_finishing = finishing.get("finish", [])
-        start = finishing.get("start").pop()
-        print(list_of_states)
-        print(list_of_symbols)
-        print("start: %s" %start)
-        print(list_of_finishing)
-        fst = FST()
-        for item in list_of_states:
-            fst.add_state(item)
-        for item in list_of_symbols:
-            fst.add_symbol(item)
-        for item in list_of_rules:
-            fst.add_rule(item[0], item[1], item[2])
-        fst.set_starting(start)
-        for item in list_of_finishing:
-            print("WTF", item)
-            fst.set_finishing(item)
-        print("is WFSA: %s" %fst.is_WFSA())
-        if not args.find_non_finishing:
-            args.output.write(repr(fst))
-        else:
-            args.output.write(fst.find_non_terminating())
+    # Evaluate FSM
+    if not fsm.is_WSFA():
+        sys.stderr.write("Not well specified finit automata\n")
+        sys.exit(62)
+    if args.minimize:
+        # TODO
+        fsm.minimize()
+    if args.analyze_string:
+        if args.case_insensitive:
+            args.analyze_string = args.analyze_string.lower()
+        correct = fsm.read_string(args.analyze_string)
+        args.output.write("1" if correct else "0")
+    elif not args.find_non_finishing:
+        args.output.write(repr(fsm))
+    else:
+        args.output.write(fsm.find_non_terminating())
     args.input.close()
     args.output.close()
     return 0
--- stateMachine.py
+++ stateMachine.py
@@ -4,6 +4,7 @@
 
 from copy import copy
 
+
 class Redefinition(BaseException):
     pass
 
@@ -22,9 +23,18 @@ class NotFinishing(BaseException):
 
 class FiniteStateMachine(object):
 
-    def __init__(self, rules_only=False, line_comment="#", post_comment=" "):
-        self.states = dict()
+    def __init__(
+        self,
+        rules_only=False,
+        line_comment="#",
+        post_comment=" ",
+        allow_redefinition=True,
+        escape_state="left_ap"
+    ):
         self.rules_only = rules_only
+        self.can_redef = allow_redefinition
+        self.literally = escape_state
+        self.states = dict()
         self.alphabet = list()
         self.current = None
         self.buffer = str()
@@ -39,42 +49,83 @@ class FiniteStateMachine(object):
         self.dump_rules = dict()
         self.finishing = list()
 
+    def __repr__(self):
+        # states
+        states = list(self.states.keys())
+        states.sort()
+        states = ", ".join(states)
+        # alphabet
+        self.alphabet.sort()
+        alphabet = list()
+        for symbol in self.alphabet:
+            alphabet.append("'%s'" % symbol)
+        alphabet = ", ".join(alphabet)
+        # rules
+        rules = list()
+        for state_name in self.states:
+            state = self.states[state_name]
+            for symbol in state.rules:
+                target_name = state.rules[symbol][0].get_name()
+                rules.append("%s '%s' -> %s" %
+                             (state_name, symbol, target_name))
+        rules.sort()
+        rules = ",\n".join(rules)
+        # starting
+        starting = self.current.get_name()
+        # finishing
+        self.finishing.sort()
+        finishing = ", ".join(self.finishing)
+        # concatenate
+        return ("(\n{%s},\n{%s},\n{\n%s\n},\n%s,\n{%s}\n)" %
+                (states, alphabet, rules, starting, finishing))
+
+    __str__ = __repr__
+
     def add_state(self, name, finishing=False):
         if (name in self.states):
-            if (self.rules_only):
+            if self.rules_only or self.can_redef:
                 return False
-            raise Redefinition(name)
+            raise Redefinition("Multiple states with name: %s" % name, "\n")
+
         self.states[name] = State(name, finishing)
         return True
 
-    def add_rule(self, starting, target, symbol, returning=False):
+    def add_rule(self, starting_name, target_name, symbol, returning=False):
         if not self.rules_only:
-            if (starting not in self.states or
-                        target not in self.states or
-                        symbol not in self.alphabet
+            if (starting_name not in self.states or
+                    target_name not in self.states or
+                    symbol not in self.alphabet
                     ):
-                raise Invalid("Missing state or symbol in definition")
+                raise Invalid("Missing state or symbol in definition", "\n")
         else:
-            self.add_state(starting)
-            self.add_state(target)
+            self.add_state(starting_name)
+            self.add_state(target_name)
             self.add_symbol(symbol)
-        self._add_dump_rule(starting, target)
+        target = self.states[target_name]
         symbol = self._symbol_decode(symbol)
-        target = self.states[target]
-        return self.states[starting].add_rule(symbol, target, returning)
+        retval = self.states[starting_name].add_rule(symbol, target, returning)
+        if retval:
+            self._add_dump_rule(starting_name, target_name)
+        return retval
 
     def add_symbol(self, symbol):
         if (self._symbol_collision(symbol)) and not self.rules_only:
-            raise Redefinition(symbol)
+            if self.can_redef:
+                return False
+            raise Redefinition("Symbol %s already declared" % symbol, "\n")
         if type(symbol) is tuple or type(symbol) is list:
             self.alphabet += symbol
         else:
             self.alphabet.append(symbol)
+        return True
 
     def set_finishing(self, name):
-        self.finishing.append(name)
-        state = self.states[name]
-        state.set_finishing()
+        if name in self.states:
+            self.finishing.append(name)
+            state = self.states[name]
+            state.set_finishing()
+        else:
+            raise Invalid("Finishing doens't exists", "\n")
 
     def _symbol_collision(self, symbol):
         if (type(symbol) is not tuple and type(symbol) is not list):
@@ -82,10 +133,11 @@ class FiniteStateMachine(object):
                 return True
         return False
 
-    def _symbol_decode(self, symbol):
-        if len(symbol) > 1 and symbol[0] == "!":
-            return SymbolGroup(symbol)
-        return symbol
+    def set_starting(self, name):
+        if name in self.states:
+            self.current = self.states[name]
+        else:
+            raise Invalid("Starting state doesn't exists", "\n")
 
     def build_from_config(self, conf):
         self.rules_only = True
@@ -114,81 +166,10 @@ class FiniteStateMachine(object):
                     ret = trans[1]
                 self.add_rule(name, target, trans[0], ret)
 
-    def set_starting(self, name):
-        self.current = self.states[name]
-
-    def step(self, char):
-        if self.line_comment == char:
-            if self.current.get_name() != "left_ap":
-                self.in_line_comment = True
-                return self.current
-        if self.in_line_comment:
-            if char == "\n":
-                self.in_line_comment = False
-                return self.step(self.post_comment)
-            return self.current
-
-        if not self.current:
-            raise Invalid("Starting state not set")
-
-        retval = self.current.step(char)
-        if retval is None:
-            raise MissingRule("Missing rule for: '%s' ord(%s) at state: %s"
-                              % (char, ord(char), self.current.get_name()))
-        self.current = retval[0]
-        self.handle_output(char, retval[1])
-        return retval[0]
-
-    def handle_output(self, char, handler):
-        if self.last_handler and self.last_handler != handler:
-            if self.last_handler not in self.output:
-                self.output[self.last_handler] = list()
-            self.output[self.last_handler].append(self.buffer)
-            self.buffer = str()
-        if handler:
-            self.buffer += char
-        self.last_handler = handler
-
-    def get_output(self):
-        return self.output
-
-    def read_string(self, string):
-        try:
-            for char in string:
-                self.step(char)
-            return True
-        except MissingRule:
-            return False
-
     def is_finishing(self):
         return self.current.is_finishing()
 
-    def _add_dump_rule(self, state, target):
-        if state not in self.dump_rules:
-            self.dump_rules[state] = list()
-        if target not in self.dump_rules[state]:
-            self.dump_rules[state].append(target)
-
-        if target not in self.reversed_rules:
-            self.reversed_rules[target] = list()
-        if state not in self.reversed_rules[target]:
-            self.reversed_rules[target].append(state)
-
-    def _all_accessible(self):
-        accessible = list(self.current.get_name())
-        while True:
-            changed = False
-            for state in accessible:
-                targets = self.dump_rules.get(state, [])
-                for target in targets:
-                    if target not in accessible:
-                        accessible.append(target)
-                        changed = True
-            if not changed:
-                break
-        return len(accessible) == len(self.states)
-
-    def is_WFSA(self):
+    def is_WSFA(self):
         starting = list(self.current.get_name())
         state_count = len(self.states)
         symbol_count = len(self.alphabet)
@@ -229,37 +210,82 @@ class FiniteStateMachine(object):
                     return False
         return non_terminating
 
-    def __repr__(self):
-        # states
-        states = list(self.states.keys())
-        states.sort()
-        states = ", ".join(states)
-        # alphabet
-        self.alphabet.sort()
-        alphabet = list()
-        for symbol in self.alphabet:
-            alphabet.append("'%s'" % symbol)
-        alphabet = ", ".join(alphabet)
-        # rules
-        rules = list()
-        for state_name in self.states:
-            state = self.states[state_name]
-            for symbol in state.rules:
-                target_name = state.rules[symbol][0].get_name()
-                rules.append("%s '%s' -> %s" %
-                             (state_name, symbol, target_name))
-        rules.sort()
-        rules = ",\n".join(rules)
-        # starting
-        starting = self.current.get_name()
-        # finishing
-        self.finishing.sort()
-        finishing = ", ".join(self.finishing)
-        # concatenate
-        return ("(\n{%s},\n{%s},\n{\n%s\n},\n%s,\n{%s}\n)" %
-            (states, alphabet, rules, starting, finishing))
+    def minimize(self):
+        pass
 
-    __str__ = __repr__
+    def step(self, char):
+        if self.line_comment == char:
+            if self.literally not in self.current.get_name():
+                self.in_line_comment = True
+                return self.current
+        if self.in_line_comment:
+            if char == "\n":
+                self.in_line_comment = False
+                return self.step(self.post_comment)
+            return self.current
+
+        if not self.current:
+            raise Invalid("Starting state not set", "\n")
+
+        retval = self.current.step(char)
+        if retval is None:
+            raise MissingRule("Missing rule for: '%s' ord(%s) at state: %s\n"
+                              % (char, ord(char), self.current.get_name()))
+        self.current = retval[0]
+        self._handle_output(char, retval[1])
+        return retval[0]
+
+    def _symbol_decode(self, symbol):
+        if len(symbol) > 1 and symbol[0] == "!":
+            return SymbolGroup(symbol)
+        return symbol
+
+    def _handle_output(self, char, handler):
+        if self.last_handler and self.last_handler != handler:
+            if self.last_handler not in self.output:
+                self.output[self.last_handler] = list()
+            self.output[self.last_handler].append(self.buffer)
+            self.buffer = str()
+        if handler:
+            self.buffer += char
+        self.last_handler = handler
+
+    def get_output(self):
+        return self.output
+
+    def read_string(self, string):
+        try:
+            for char in string:
+                self.step(char)
+            return self.is_finishing()
+        except MissingRule:
+            return False
+
+    def _add_dump_rule(self, state, target):
+        if state not in self.dump_rules:
+            self.dump_rules[state] = list()
+        if target not in self.dump_rules[state]:
+            self.dump_rules[state].append(target)
+
+        if target not in self.reversed_rules:
+            self.reversed_rules[target] = list()
+        if state not in self.reversed_rules[target]:
+            self.reversed_rules[target].append(state)
+
+    def _all_accessible(self):
+        accessible = list(self.current.get_name())
+        while True:
+            changed = False
+            for state in accessible:
+                targets = self.dump_rules.get(state, [])
+                for target in targets:
+                    if target not in accessible:
+                        accessible.append(target)
+                        changed = True
+            if not changed:
+                break
+        return len(accessible) == len(self.states)
+
 
 class State(object):
 
@@ -270,8 +296,15 @@ class State(object):
 
     def add_rule(self, symbol, target, returning):
         if symbol in self.rules:
-            raise Redefinition("%s in %s" % (symbol, self.name))
+            if target is self.rules[symbol][0]:
+                if can_redef:
+                    return False
+                raise Redefinition("%s in %s" % (symbol, self.name), "\n")
+            else:
+                raise Invalid(
+                    "Multiple targets for one symbol... nondeterminism", "\n")
         self.rules[symbol] = (target, returning)
+        return True
 
     def finishing(self):
         return self.finishing
